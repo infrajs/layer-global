eadbcaf5130cc940ba9c83e383752cbdd48d4c82
Anton <a.kiyatkin@itlf.ru>
2016-06-29 13:09:51 +0400

    fix Controller.Global

--- global.js
+++ global.js
@@ -1,80 +1,82 @@
 (function () {
 	//global:(bool);// проверка есть tpl или нет. Если tpl будет загружен пустой слой не покажется
 	//globalignoredata:(bool); загружать повторно данные или нет
-	Event.one('Infrajs.oninit',function(){
-		infrajs.externalAdd('global','external');
-		infrajs.parsedAdd(function(layer){
-			if(!layer.global)return '';
-			var s='';
-			infra.fora(layer.global,function(g){
-				g=infrajs.global.get(g);
-				s+=g.value+':';
+	Event.one('Controller.oninit', function () {
+		Controller.externalAdd('global','external');
+		Controller.parsedAdd(function(layer){
+			if (!layer.global) return '';
+			var s = '';
+			Each.exec(layer.global, function (g) {
+				g = Global.get(g);
+				s += g.value + ':';
 			});
 			return s;
 		});
 	});
 
-	infrajs.global={
+	Global = {
 		globals:{},
-		get:function(name){
-			if(!this.globals[name]){
-				this.globals[name]={
-					value:0,
-					unloads:{},
-					layers:[]
+		get: function (name) {
+			if (!Global.globals[name]) {
+				Global.globals[name] = {
+					value: 0,
+					unloads: {},
+					layers: {}
 				};
 			}
-			return this.globals[name];
+			return Global.globals[name];
 		},
-		unload:function(name,path){
-			infra.fora(name,function(n){
-				var g=this.get(n);
+		unload: function (name, path) {
+			Each.exec(name, function (n) {
+				var g = Global.get(n);
 				g.unloads[path]=true;
-			}.bind(this));
+			});
 		},
-		counter:1,
-		set:function(names){
-			infra.fora(names,function(name){
-				var g=this.get(name);
-				g.value=this.counter++;
-				for(var path in g.unloads){
-					infra.unload(path);
+		check: function(name) {
+			Controller.Global.set(name);
+			var ids = Controller.Global.get(name).layers;
+			var layers = [];
+			for (var i in ids) {
+				layers.push(ids[i]);
+			}
+			Controller.check(layers);
+		},
+		counter: 1,
+		set: function (names) {
+			Each.exec(names, function (name) {
+				var g = Global.get(name);
+				g.value = Global.counter++;
+				for (var path in g.unloads) {
+					Load.unload(path);
 				}
-				//Event.onext('Infrajs.onshow', function(){
-					for(var i=0,l=g.layers.length;i<l;i++){
-						var layer=g.layers[i];
-						if(!layer.onsubmit)continue;
-						if(!layer.config)continue;
-						if(layer.config&&layer.config.ans)layer.config.ans={};
-					}
-				//});
-			}.bind(this));
+
+				Each.exec(g.layers, function(layer){
+					if (!layer.onsubmit) return;
+					if (!layer.config) return;
+					if (layer.config && layer.config.ans) layer.config.ans = {};
+				});
+			});
 		}
 		//,onsubmit:[]
 	}
-	infrajs.checkGlobal=function(layer){
-		if(!layer.global)return;
-		//if(layer.globaled)return;
-		//layer.globaled=true;
-		/*if(layer.onsubmit){
-			infrajs.global.onsubmit.push(layer);
-		}*/
-		var json='';
+	infrajs.checkGlobal = function (layer) {
+		if (!layer.global) return;
+		var json = '';
 		if(layer.json){
-			if(layer.json.constructor==Array){
-				json=layer.json[0];
-			}else{
-				json=layer.json;
+			if (layer.json.constructor == Array) {
+				json = layer.json[0];
+			} else {
+				json = layer.json;
 			}
 		}
-		infra.fora(layer.global,function(n){
-			var g=infrajs.global.get(n);
-			if(json){
-				g.unloads[json]=true;
+		Each.exec(layer.global, function (n) {
+			var g = Global.get(n);
+			if (json) {
+				g.unloads[json] = true;
 			}
-			//if(layer.onsubmit){
-				g.layers.push(layer);
-			//}
+			g.layers[layer.id] = layer;
 		});
 	}
+	Controller.Global = Global;
+	infrajs.global = Global;
 })();
