81d89de57cc02fb574ddbfeb47b2ef88f46cbdf4
Anton Kiyatkin <a.kiyatkin@itlf.ru>
2020-05-07 10:49:39 +0400

    fix

--- Global.js
+++ Global.js
@@ -1,69 +1,58 @@
-(function () {
-	//global:(bool);// проверка есть tpl или нет. Если tpl будет загружен пустой слой не покажется
-	//globalignoredata:(bool); загружать повторно данные или нет
-	Event.one('Controller.oninit', function () {
-		Controller.externalAdd('global','external');
-		Controller.parsedAdd(function(layer){
-			if (!layer.global) return '';
-			var s = '';
-			Each.exec(layer.global, function (g) {
-				g = Global.get(g);
-				s += g.value + ':';
-			});
-			return s;
-		});
-	});
-
-	window.Global = {
-		globals:{},
-		get: function (name) {
-			if (!Global.globals[name]) {
-				Global.globals[name] = {
-					value: 0,
-					unloads: {},
-					layers: {}
-				};
-			}
-			return Global.globals[name];
-		},
-		unload: function (name, path) {
-			Each.exec(name, function (n) {
-				var g = Global.get(n);
-				g.unloads[path]=true;
-			});
-		},
-		check: function(name) {
-			Controller.Global.set(name);
-			var ids = Controller.Global.get(name).layers;
-			var layers = [];
-			for (var i in ids) {
-				layers.push(ids[i]);
-			}
-			Controller.check(layers);
-			Controller.check();
-		},
-		counter: 1,
-		set: function (names) {
-			Each.exec(names, function (name) {
-				var g = Global.get(name);
-				g.value = Global.counter++;
-				for (var path in g.unloads) {
-					Load.unload(path);
-				}
-
-				Each.exec(g.layers, function(layer){
-					if (!layer.onsubmit) return;
-					if (!layer.config) return;
-					if (layer.config && layer.config.ans) layer.config.ans = {};
-				});
-			});
+let Global = {
+	globals:{},
+	get: function (name) {
+		if (!Global.globals[name]) {
+			Global.globals[name] = {
+				value: 0,
+				unloads: {},
+				layers: {}
+			};
 		}
-		//,onsubmit:[]
-	}
-	infrajs.checkGlobal = function (layer) {
-		if (!layer.global) return;
+		return Global.globals[name];
+	},
+	unload: function (name, path) {
+		Each.exec(name, function (n) {
+			var g = Global.get(n);
+			g.unloads[path]=true;
+		});
+	},
+	hash: function (global) {
+		var s = '';
+		Each.exec(global, function (g) {
+			g = Global.get(g);
+			s += g.value + ':';
+		});
+		return s
+	},
+	check: function(name) {
+		Global.set(name);
+		var ids = Global.get(name).layers;
+		var layers = [];
+		for (var i in ids) {
+			layers.push(ids[i]);
+		}
+		Controller.check(layers);
+		//Controller.check();
+	},
+	counter: 1,
+	set: function (names) {
+		Each.exec(names, function (name) {
+			var g = Global.get(name);
+			g.value = Global.counter++;
+			for (var path in g.unloads) {
+				Load.unload(path);
+			}
+
+			Each.exec(g.layers, function(layer){
+				if (!layer.onsubmit) return;
+				if (!layer.config) return;
+				if (layer.config && layer.config.ans) layer.config.ans = {};
+			});
+		});
+	},
+	checkLayer: function (layer) {
 		var json = '';
-		if(layer.json){
+		if (layer.json){
 			if (layer.json.constructor == Array) {
 				json = layer.json[0];
 			} else {
@@ -78,6 +67,12 @@
 			g.layers[layer.id] = layer;
 		});
 	}
-	Controller.Global = Global;
-	infrajs.global = Global;
-})();
+}
+
+
+window.Global = Global
+Controller.Global = Global
+infrajs.global = Global
+
+export {Global}
+export default Global
--- infra.js
+++ infra.js
@@ -1,5 +1,18 @@
+import Fire from "/vendor/akiyatkin/load/Fire.js"
 
-Event.handler('Layer.onshow', function (layer){
-	//global
-	infrajs.checkGlobal(layer);
-},'global:tpl');
+Fire.hand(Controller, 'oninit', async () => {
+	Controller.externalAdd('global', 'external');
+	let { Global } = await import('/vendor/infrajs/layer-global/Global.js')
+	Controller.parsedAdd(layer => {
+		if (!layer.global) return;
+		
+		let hash = Global.hash(layer.global)
+		return hash;
+	});
+})
+
+Event.handler('Layer.onshow', async (layer) => {
+	if (!layer.global) return;
+	let { Global } = await import('/vendor/infrajs/layer-global/Global.js')
+	Global.checkLayer(layer);
+}, 'global:tpl');
