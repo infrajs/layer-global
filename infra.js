282837d282e74c24a8564b02fdc0328fcb14715a
Anton Kiyatkin <a.kiyatkin@itlf.ru>
2020-05-12 20:30:56 +0400

    fix

--- infra.js
+++ infra.js
@@ -1,18 +1,7 @@
-import Fire from "/vendor/akiyatkin/load/Fire.js"
-import Global from "/vendor/infrajs/layer-global/Global.js"
+import { Controller } from '/vendor/infrajs/controller/src/Controller.js'
 
-Fire.hand(Controller, 'init', async () => {
-	Controller.externalAdd('global', 'external');
-	Controller.parsedAdd(layer => {
-		if (!layer.global) return;
-		let hash = Global.hash(layer.global)
-		return hash;
-	});
+Controller.hand('init', async () => {
+	await import('./init.js')
 })
-Controller.Global = Global
-infrajs.global = Global
-window.Global = Global
-Event.handler('Layer.onshow', async (layer) => {
-	if (!layer.global) return;
-	Global.checkLayer(layer);
-}, 'global:tpl');
+
+
---
+++ init.js
@@ -0,0 +1,30 @@
+import { Global } from "/vendor/infrajs/layer-global/Global.js"
+import { Parsed } from '/vendor/infrajs/controller/src/Parsed.js'
+import { External } from '/vendor/infrajs/controller/src/External.js'
+import { Controller } from '/vendor/infrajs/controller/src/Controller.js'
+import { Event } from '/vendor/infrajs/event/Event.js'
+
+/*
+2 инициализации
+1 - onload - при заходе на страницу
+2 - Обращение к классу-файлу - при клике - через события (подписанное в collect) Controller.hand('init'
+
+*/
+
+
+Controller.hand('init', async () => {
+	External.add('global', 'external');
+	Parsed.add(layer => {
+		if (!layer.global) return;
+		let hash = Global.hash(layer.global)
+		return hash;
+	});
+	Event.handler('Layer.onshow', async (layer) => {
+		if (!layer.global) return;
+		Global.checkLayer(layer);
+	}, 'global:tpl');
+})
+
+
+Controller.Global = Global
+window.Global = Global
