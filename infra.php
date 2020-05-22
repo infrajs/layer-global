638f590f6908802701e8d425a6e273eb7ebbae01
Anton Kiyatkin <a.kiyatkin@itlf.ru>
2020-05-22 09:49:20 +0400

    fix

--- infra.php
+++ infra.php
@@ -1,6 +1,16 @@
 <?php
 use infrajs\controller\Layer;
 use infrajs\event\Event;
+use infrajs\each\Each;
 Event::handler('Controller.oninit', function () {
-	Layer::parsedAdd('global');
+	Layer::parsedAdd(function ($layer){
+		if (empty($layer['global'])) return '';
+		$hash = '';
+		Each::exec($layer['global'], function &($g) use (&$hash) {
+			$hash .= '0:';
+			$r = null;
+			return $r;
+		});
+		return $hash;
+	});
 });
